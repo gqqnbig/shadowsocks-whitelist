13a3a9410ad733de635923d3b9c213fd115decf8
QQ喵 <gqqnb2005@gmail.com>
2023-04-19 11:47:44 +0800

    Implement a custom formatter for eslint for it to output the lines being reported

---
+++ customFormatter.js
@@ -0,0 +1,28 @@
+module.exports = function (results) {
+  let output = '';
+  results.forEach((result) => {
+    const { filePath, messages, source } = result;
+
+    if (messages.length > 0) {
+      const fileContent = source ? source.split('\n') : [];
+      output += `\n${filePath}\n`;
+
+      messages.forEach((message) => {
+        const { line, column, ruleId, message: msg, severity } = message;
+        const errorWarning = severity === 1 ? 'warning' : 'error';
+        output += `  ${line}:${column}  ${errorWarning}  ${msg} (${ruleId})\n`;
+
+        // Display the source code line with the error
+        const sourceLine = fileContent[line - 1];
+        //output += `Source is ${sourceLine}\n`;
+        if (sourceLine) {
+          const pointer = ' '.repeat(column - 1) + '^';
+          output += `  ${sourceLine}\n  ${pointer}\n`;
+        }
+      });
+    }
+  });
+
+  return output;
+};
+
